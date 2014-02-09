fbb4408459fdd749ecb90db67d9c0af57c9e4944
nunojorge <nujo@hotmail.com>
2014-02-09 16:10:48 -0500

    Restaurant model and user model are created, in addition to their controllers and views

---
+++ bitmaker/seatyourself/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seatyourself::Application.config.secret_key_base = '8c956d488e0ef7ff6aa236a9eab7ac885a195c099110d42c037331d9fd931c08bc8020093de90ac05edc595a62a3f1b9684db9fe49fe266455c003d7cecc9437'
